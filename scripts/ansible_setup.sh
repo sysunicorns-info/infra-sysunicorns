03c68cc08c881b3e160db02ed7404f9d612d4925
miragecentury <victorien.vanroye@gmail.com>
2022-10-24 20:23:13 +0200

    #1 install pre-requisite

---
+++ scripts/ansible_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo /opt/ansible-runtime/bin/python -m pip install bcrypt
+
+sudo apt-get install sshpass
+
+# Use to generate ssh-key pair
+ansible-galaxy collection install community.crypto
+ansible-galaxy collection install ansible.posix
